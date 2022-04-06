2338b610931fbca1ee520057b73339640fb0b03e
Kevin Valdivia Alfonso <kevin.dfrontend@gmail.com>
2022-04-06 15:51:57 -0500

    BugFix portafolio pdf view

--- src/pages/ViewPdf.js
+++ src/pages/ViewPdf.js
@@ -1,36 +1,38 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 const ui = {
-  PdfContainer: styled.div`
-    margin-top: 100px;
-    height: calc(100vh - 100px);
+	PdfContainer: styled.div`
+		margin-top: 100px;
+		height: calc(100vh - 100px);
 
-    a {
-      background-color: #ffe700;
-    }
-  `
-}
+		a {
+			background-color: #ffe700;
+		}
+	`
+};
 
 const ViewPdf = () => {
-  useEffect(() => {
-    if (typeof window.orientation !== 'undefined') {
-      document.getElementById('enlace-descarga').click()
-      window.close()
-    }
-  }, [])
-  return (
-    <ui.PdfContainer >
-      <object
-        data={require('../assets/docs/Kevin_Valdivia_Alfonso_CV.pdf')}
-        type='application/pdf'
-        width='100%'
-        height='100%'
-      >
-        <a href={require('../assets/docs/Kevin_Valdivia_Alfonso_CV.pdf')} id='enlace-descarga'>Tu dispositivo no puede visualizar el documento, Click para descargarlo</a>
-      </object>
-    </ui.PdfContainer>
-  )
-}
+	useEffect(() => {
+		if (typeof window.orientation !== 'undefined') {
+			document.getElementById('enlace-descarga').click();
+			window.close();
+		}
+	}, []);
+	return (
+		<ui.PdfContainer>
+			<object
+				data={require('../assets/docs/Currículum_Kevin_Valdivia_Alfonso.pdf')}
+				type="application/pdf"
+				width="100%"
+				height="100%"
+			>
+				<a href={require('../assets/docs/Currículum_Kevin_Valdivia_Alfonso.pdf')} id="enlace-descarga">
+					Tu dispositivo no puede visualizar el documento, Click para descargarlo
+				</a>
+			</object>
+		</ui.PdfContainer>
+	);
+};
 
-export default ViewPdf
+export default ViewPdf;
